ebadb1b5cbd056bc5b4b621aa9df86b1eacac849
Techno75 <gdepretz@gmail.com>
2020-05-25 18:52:52 +0200

    Mise à jour de la fonction pour fetch la data sur l'api, en fonction de si il y a une authorisation ou pas.

--- src/Functions/FetchData.js
+++ src/Functions/FetchData.js
@@ -1,43 +1,83 @@
 /*
-*** Route = string
+*** Route = string ('url')
 *** Methode = string ('POST', 'GET', ...)
-*** body = object     // si utilisation de méthode Post
+*** body = object (si utilisation de méthode Post, ...)
+*** authorization = string ('Token', si l'utilisateur est connecté)
 */
 
 const fetchData = (route, method, body, authorization) => {
+  if (authorization === undefined) {
     if (body === undefined) {
-      return fetch(route,  {mode: 'cors', method: method,
+      return fetch(route, {mode: 'cors', method: method,
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin' : "*",
-          'Authorization': authorization,
+          'Access-Control-Allow-Origin' : '*',
         },
       })
       .then((res)=>{
           if (res.status !== 200) {
-             return;
+             console.log(res.status);
+             return res.status;
           }
-          return res.json()
+          return res.json();
         })
     }
     else {
-      return fetch(route,  {mode: 'cors', method: method,
+      return fetch(route, {mode: 'cors', method: method,
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin' : "*",
-          'Authorization': authorization,
+          'Access-Control-Allow-Origin' : '*',
         },
         body : JSON.stringify(body)
       })
       .then((res)=>{
           if (res.status !== 200) {
-             return;
+             console.log(res.status);
+             return res.status;
           }
-          return res.json()
+          return res.json();
         })
     }
   }
+  else {
+    if (body === undefined) {
+      return fetch(route, {mode: 'cors', method: method,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin' : '*',
+          'Authorization': authorization
+        },
+      })
+      .then((res)=>{
+          if (res.status !== 200) {
+             console.log(res.status);
+             return res.status;
+          }
+          return res.json();
+        })
+    }
+    else {
+      return fetch(route, {mode: 'cors', method: method,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin' : '*',
+          'Authorization': authorization
+        },
+        body : JSON.stringify(body)
+      })
+      .then((res)=>{
+          if (res.status !== 200) {
+             console.log(res.status);
+             return res.status;
+          }
+          return res.json();
+        })
+    }
+  }
+}
 
 export default fetchData;
